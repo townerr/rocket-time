86c4b00769519a88a45ff99e98dc5afc3fdc4504
townerr <townerr@yahoo.com>
2025-01-28 12:01:18 -0600

    added neon to prisma

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,9 +1,19 @@
 import { PrismaClient } from "@prisma/client";
-
+import { PrismaNeon } from '@prisma/adapter-neon';
+import { Pool, neonConfig } from '@neondatabase/serverless';
 import { env } from "~/env";
+
+//setup neon with prisma
+import ws from 'ws';
+neonConfig.webSocketConstructor = ws;
+
+const connectionString = `${env.DATABASE_URL}`;
+const pool = new Pool({ connectionString });
+const adapter = new PrismaNeon(pool);
 
 const createPrismaClient = () =>
   new PrismaClient({
+    adapter,
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
   });

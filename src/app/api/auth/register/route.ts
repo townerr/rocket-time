1cbe8d4943f9b55c553876e91f45d03743d7b8a0
Ryan Towner <townerr@yahoo.com>
2025-10-26 21:48:04 -0500

    comment out unused register route

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import { db } from "~/server/db";
 
 export async function POST(request: Request) {
-    const bcrypt = require('bcrypt');
+    /*const bcrypt = require('bcrypt');
     try {
         const { firstname, lastname, email, password } = await request.json();
         const hashedPassword = await bcrypt.hash(password, 10);
@@ -18,5 +18,5 @@ export async function POST(request: Request) {
         console.log({ e });
     }
 
-    return NextResponse.json({ message: "success" });
+    return NextResponse.json({ message: "success" });*/
 }
